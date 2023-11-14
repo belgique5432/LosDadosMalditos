e82f352462ce79af56d4886734a847b3fc8b7f9a
belgique5432 <105752407+belgique5432@users.noreply.github.com>
2023-11-14 11:59:48 -0300

    Add files via upload

--- utils.h
+++ utils.h
@@ -7,7 +7,6 @@
 using namespace std;
 
 extern int dado[];
-
 int mostrarDados()
 {
     rlutil::setColor(rlutil::MAGENTA);
@@ -158,12 +157,13 @@ int mostrarDados()
             cout << " -------   ";
         }
     }
-rlutil::setColor(rlutil::WHITE);
+    rlutil::setColor(rlutil::WHITE);
 }
+
 int unJugador()
 {
     string jugador1,mj;
-    int contadorTiradas = 0, puntuacion = 0, rondas = 1;
+    int contadorTiradas = 0, puntuacion = 0, rondas = 1, contador;
     int contadorEscalera = 0, sexteto = 0;
     int contadorTrio = 0, TX=0, MAX1=0, MAX2=0,mp=0;
 
@@ -178,7 +178,6 @@ int unJugador()
         MAX1=0, MAX2=0;
 
 
-        bool sonIguales = true;
         srand(time(NULL)); // Inicializa la semilla para la generación de números aleatorios.
         for (int i = 0; i <= 5; i++)
         {
@@ -187,15 +186,19 @@ int unJugador()
             {
                 contadorEscalera++;
             }
-            puntuacion += dado[i];
+            if (dado[i] == 6)
+            {
+                sexteto++;
+            }
+
         }
 // CODIGO TRIO X CODIGO TRIO X CODIGO TRIO X CODIGO TRIO X
-        for (int N = 1; N < 7; N++)
+        for (int N = 0; N < 6; N++)
         {
-            contadorTrio=0;
+            contadorTrio= 0;
             for(int D = 0; D <= 5; D++)
             {
-                if (dado[D] == N)
+                if (dado[D] == dado[N])
                 {
                     contadorTrio ++;
                 }
@@ -204,6 +207,7 @@ int unJugador()
                     if (MAX1==0)
                     {
                         MAX1=dado[D];
+                        contadorTrio = 0;
                     }
                     else
                     {
@@ -223,6 +227,7 @@ int unJugador()
 
 
 
+
         cout << "Ronda: " << rondas << endl;
         contadorTiradas++;
         mostrarDados();
@@ -230,11 +235,10 @@ int unJugador()
         if (contadorTiradas % 3 == 0)
         {
             rondas++;
-
-           system("cls");
+            system("cls");
             rlutil::setColor(rlutil::BLUE);
             rlutil::locate(44,3);
-          cout<<"-----------------------------------------";
+            cout<<"-----------------------------------------";
             rlutil::locate(44,5);
             cout<<" jugador "<<jugador1<<" suiguente ronda "<<rondas<< " puntaje "<<puntuacion<<endl;
             rlutil::locate(44,7);
@@ -244,13 +248,27 @@ int unJugador()
             system("pause");
             rlutil::setColor(rlutil::WHITE);
             system("cls");
-             }
+
         }
 
         if (contadorEscalera == 6)
         {
             cout << "Victoria por escalera" << endl;
             puntuacion = 501;
+        }
+
+        if (sexteto == 6)
+        {
+            cout<<"Sexteto!, puntuacion vuelve a 0"<<endl;
+            puntuacion = 0;
+        }
+
+        if (contadorTrio < 3)
+        {
+            for (int x = 0; x < 6; x++)
+            {
+                puntuacion+= dado[x];
+            }
         }
 
 
@@ -271,6 +289,8 @@ int unJugador()
 
     return 0; // Puedes devolver un valor si es necesario
 }
+
+
 int MP(int& mp, string& mj)
 {
     if(mp==0)
@@ -287,10 +307,11 @@ int MP(int& mp, string& mj)
 
 
 }
+
 int dosJugadores()
 {
     string jugador1, jugador2;
-    int puntuacionJugador1 = 0, puntuacionJugador2 = 0, contadorTiradas = 0, rondas = 1;
+    int puntuacionJugador1 = 0, puntuacionJugador2 = 0, contadorTiradas = 0, rondas = 0;
     bool bandera = true;
 
 
@@ -302,7 +323,7 @@ int dosJugadores()
     while (puntuacionJugador1 <= 500 || puntuacionJugador2 <= 500)
     {
 
-        int contadorEscalera = 0, sexteto = 0;
+        int contadorEscalera = 0, sexteto = 0, contador = 0;
         int contadorTrio = 0, TX=0, MAX1=0, MAX2=0;
 
 
@@ -311,20 +332,18 @@ int dosJugadores()
         sexteto = 0;
         MAX1=0, MAX2=0;
 
-            if (contadorTiradas % 6 == 0) {
-                    rondas++;
-
-
-
-           system("cls");
+        if (contadorTiradas % 6 == 0)
+        {
+            if (rondas != 0) {
+                                system("cls");
             rlutil::setColor(rlutil::YELLOW);
             rlutil::locate(44,3);
-          cout<<"-----------------------------------------";
+            cout<<"-----------------------------------------";
             rlutil::locate(44,5);
             cout<<" suiguente ronda "<<rondas<<endl;
-             rlutil::locate(44,6);
+            rlutil::locate(44,6);
             cout<<" jugador "<<jugador1<< " puntaje "<<puntuacionJugador1<<endl;
-             rlutil::locate(44,7);
+            rlutil::locate(44,7);
             cout<<" jugador "<<jugador2<< " puntaje "<<puntuacionJugador2<<endl;
             rlutil::locate(44,8);
             cout<<"-----------------------------------------";
@@ -333,81 +352,16 @@ int dosJugadores()
             system("pause");
             rlutil::setColor(rlutil::WHITE);
             system("cls");
-             }
-                }
+
+            }
+            rondas++;
+
+        }
 
 
-            if (bandera)
-            {
-                cout<<endl<<endl<<"Jugador 1"<<endl;
-                bool sonIguales = true;
-                srand(time(NULL)); // Inicializa la semilla para la generación de números aleatorios.
-                for (int i = 0; i <= 5; i++)
-                {
-                    dado[i] = 1 + rand() % 6;
-                    if (dado[i] == i + 1)
-                    {
-                        contadorEscalera++;
-                    }
-                    puntuacionJugador1 += dado[i];
-                }
-// CODIGO TRIO X CODIGO TRIO X CODIGO TRIO X CODIGO TRIO X
-                for (int N = 1; N < 7; N++)
-                {
-                    contadorTrio=0;
-                    for(int D = 0; D <= 5; D++)
-                    {
-                        if (dado[D] == N)
-                        {
-                            contadorTrio ++;
-                        }
-                        if(contadorTrio==3)
-                        {
-                            if (MAX1==0)
-                            {
-                                MAX1=dado[D];
-                            }
-                            else
-                            {
-                                MAX2=dado[D];
-                            }
-                            if(MAX2>MAX1)
-                            {
-                                MAX1=MAX2;
-                            }
-                        }
-
-                    }
-                }
-                puntuacionJugador1 += MAX1*10;
-
-// CODIGO TRIO X  CODIGO TRIO X CODIGO TRIO X CODIGO TRIO X
-
-
-
-                cout << "Ronda: " << rondas << endl;
-                contadorTiradas++;
-                mostrarDados();
-
-                if (contadorEscalera == 6)
-                {
-                    cout << "Victoria por escalera" << endl;
-                    puntuacionJugador1 = 501;
-                }
-
-
-
-                // Verifica la puntuación y continúa o regresa al menú
-                if (puntuacionJugador1 < 500)
-                {
-                    cout <<endl<< "Puntuacion actual del jugador 1: " << puntuacionJugador1 << " (Necesitas 500 para ganar)" << endl;
-                    system("pause");
-                }
-            }  else
+        if (bandera)
         {
-            cout<<endl<<endl<<"Jugador 2"<<endl;
-
-            bool sonIguales = true;
+            cout<<endl<<endl<<"Jugador 1"<<endl;
             srand(time(NULL)); // Inicializa la semilla para la generación de números aleatorios.
             for (int i = 0; i <= 5; i++)
             {
@@ -416,12 +370,98 @@ int dosJugadores()
                 {
                     contadorEscalera++;
                 }
-                puntuacionJugador2 += dado[i];
+                if (dado[i] == 6) {
+                    sexteto++;
+                }
+
             }
 // CODIGO TRIO X CODIGO TRIO X CODIGO TRIO X CODIGO TRIO X
             for (int N = 1; N < 7; N++)
             {
                 contadorTrio=0;
+                for(int D = 0; D <= 5; D++)
+                {
+                    if (dado[D] == N)
+                    {
+                        contadorTrio ++;
+                    }
+                    if(contadorTrio==3)
+                    {
+                        if (MAX1==0)
+                        {
+                            MAX1=dado[D];
+                            contadorTrio = 0;
+                        }
+                        else
+                        {
+                            MAX2=dado[D];
+                        }
+                        if(MAX2>MAX1)
+                        {
+                            MAX1=MAX2;
+                        }
+                    }
+
+                }
+            }
+            puntuacionJugador1 += MAX1*10;
+
+// CODIGO TRIO X  CODIGO TRIO X CODIGO TRIO X CODIGO TRIO X
+
+
+
+            cout << "Ronda: " << rondas << endl;
+            mostrarDados();
+
+            if (contadorEscalera == 6)
+            {
+                cout << "Victoria por escalera" << endl;
+                puntuacionJugador1 = 501;
+            }
+
+            if (contadorTrio < 3)
+            {
+                for (int x = 0; x < 6; x++)
+                {
+                    puntuacionJugador1+= dado[x];
+                }
+            }
+
+            if (sexteto == 6)
+            {
+                cout<<"Sexteto! la puntuacion del jugador 1 volvera a 0";
+                puntuacionJugador1 = 0;
+            }
+
+
+
+            // Verifica la puntuación y continúa o regresa al menú
+            if (puntuacionJugador1 < 500)
+            {
+                cout <<endl<< "Puntuacion actual del jugador 1: " << puntuacionJugador1 << " (Necesitas 500 para ganar)" << endl;
+                system("pause");
+            }
+        }
+        else
+        {
+            cout<<endl<<endl<<"Jugador 2"<<endl;
+
+            srand(time(NULL)); // Inicializa la semilla para la generación de números aleatorios.
+            for (int i = 0; i <= 5; i++)
+            {
+                dado[i] = 1 + rand() % 6;
+                if (dado[i] == i + 1)
+                {
+                    contadorEscalera++;
+                }
+                if (dado[i] == 6)
+                {
+                    sexteto++;
+                }
+            }
+// CODIGO TRIO X CODIGO TRIO X CODIGO TRIO X CODIGO TRIO X
+            for (int N = 1; N < 7; N++)
+            {
                 for(int D = 0; D <= 5; D++)
                 {
                     if (dado[D] == N)
@@ -453,13 +493,27 @@ int dosJugadores()
 
 
             cout << "Ronda: " << rondas << endl;
-            contadorTiradas++;
             mostrarDados();
 
             if (contadorEscalera == 6)
             {
                 cout << "Victoria por escalera" << endl;
                 puntuacionJugador2 = 501;
+            }
+
+            if (contadorTrio < 3)
+            {
+                for (int x = 0; x < 6; x++)
+                {
+                    puntuacionJugador2+= dado[x];
+                }
+            }
+
+            if (sexteto == 6)
+            {
+                cout<<"Sexteto! La puntuacion del jugador 2 volvera a 0" <<endl;
+                puntuacionJugador2 = 0;
+
             }
 
 
@@ -471,9 +525,9 @@ int dosJugadores()
                 system("pause");
             }
         }
-            contadorTiradas++;
-            bandera = !bandera;
-}
+        contadorTiradas++;
+        bandera = !bandera;
+    }
 
 
 }
